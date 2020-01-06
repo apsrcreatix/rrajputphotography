b0f8bb428d7e740282bb9709d4b591afa8808c6a
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-01-07 01:55:04 +0530

    add testimonial data

---
+++ src/data/testimonials.js
@@ -0,0 +1,34 @@
+export const TESTIMONIAL = [
+    {
+        id: 1,
+        name: "Ram Prabhu",
+        location: "Bangalore, India",
+        tags: ["Family Photoshoot", "Kids Photoshoot"],
+        comment: "Thanks Rajesh Rajput for giving super photographs. Really enjoyed photography session with you!",
+        image: "1"
+    },
+    {
+        id: 2,
+        name: "Sridhar",
+        location: "Bangalore, India",
+        tags: ["Pre WeddingPhotoshoot", "Couple Photoshoot"],
+        comment: "Amazing it was! Thanks Rajesh. All the best",
+        image: "2"
+    },
+    {
+        id: 3,
+        name: "Ravi Rajput",
+        location: "Hoshangabad, India",
+        tags: ["Family Photoshoot", "Couple Photoshoot"],
+        comment: "I wish great success for you and really appreciate the way you do photography. Best of luck!",
+        image: "3"
+    },
+    {
+        id: 4,
+        name: "Aditya Rajput",
+        location: "Hoshangabad, India",
+        tags: ["Model Photoshoot", "Profile Pictures"],
+        comment: "I have seen his photography growing with the time. I believe that he is a awesome photographer and a mentor for those who want join him in his work.",
+        image: "4"
+    }
+];
