8994b322f14e7c9222ad9705ebe8324eb6ec5399
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-01-07 01:55:28 +0530

    add testimonials data using graphql

---
+++ src/components/testimonial.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { TESTIMONIAL } from "../data/testimonials";
+import { graphql, useStaticQuery } from "gatsby";
+
+const Testimonials = (props) => {
+
+    const data = useStaticQuery(graphql`
+    {
+      allFile(filter: {sourceInstanceName: {eq: "images"}}, sort: {fields: name}) {
+        edges {
+          node {
+            sourceInstanceName
+            childImageSharp {
+              fixed {
+                src
+                originalName
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+    const filterData = data.allFile.edges.map(node => ({
+        ...node.node.childImageSharp
+    }))
+
+    return (
+        <>
+
+            <h1 className="title">
+                Testimonials
+            </h1>
+            <div className="columns is-desktop hide-scrollbar" style={{ overflow: "auto", margin: "0 -32px" }}>
+                {TESTIMONIAL.map((value, index) => (
+                    <div className="column" key={value.image}>
+                        <div className="card" key={value.image}>
+
+                            <div className="card-content">
+                                <div className="media">
+                                    <div className="media-left">
+                                        <figure className="image is-48x48">
+                                            <Image fixed={{ src: filterData[index].fixed.src, height: 50, width: 50 }}
+                                                alt={"Image of " + value.name}
+                                                style={{ borderRadius: "50%" }}
+                                            />
+                                        </figure>
+                                    </div>
+
+                                    <div className="media-content">
+                                        <p className="title is-5">{value.name}</p>
+                                        <p className="subtitle is-6">{value.location}</p>
+                                    </div>
+                                </div>
+
+                                <div className="content">
+                                    {value.comment}
+                                    <br />
+                                    <br />
+                                    <div className="tags">
+                                        {value.tags.map(tags => <span className="tag is-info is-light" key={tags}>{tags}</span>)}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
+
+            </div>
+
+
+
+
+        </>
+
+    );
+};
+
+
+
+
+
+export default Testimonials;  
