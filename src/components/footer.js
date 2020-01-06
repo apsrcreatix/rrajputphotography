c85e12df33eeca53430de0a454383128e34cc535
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-01-07 02:39:21 +0530

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = () => {
+    return <footer class="footer" style={{ margin: "0 -32px" }}>
+        <div class="content has-text-centered is-white">
+            <p>
+                <strong>Gifted  by <a href="https://apsrcreatix.me">apsrcreatix</a> with <span role="img" aria-label="heart">❤️</span></strong>
+            </p>
+        </div>
+    </footer>
+
+}
+
+export default Layout;
