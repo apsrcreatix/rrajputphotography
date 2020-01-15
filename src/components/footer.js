1d0aca60b2dadb4109d03f946dd2e21803ca5fd8
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-01-15 12:23:00 +0530

    fix minor errors

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Layout = () => {
-    return <footer class="footer" style={{ margin: "0 -32px" }}>
-        <div class="content has-text-centered is-white">
+    return <footer className="footer" style={{ margin: "0 -32px" }}>
+        <div className="content has-text-centered is-white">
             <p>
                 <strong>Gifted  by <a href="https://apsrcreatix.me">apsrcreatix</a> with <span role="img" aria-label="heart">❤️</span></strong>
             </p>
