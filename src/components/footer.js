7386b88d0b5cf4df3c3951693bbd5262a0794961
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-01-15 17:45:03 +0530

    normalise footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Layout = () => {
-    return <footer className="footer" style={{ margin: "0 -32px" }}>
+    return <footer className="footer" style={{ margin: "0 -32px", background: "transparent" }}>
         <div className="content has-text-centered is-white">
             <p>
                 <strong>Gifted  by <a href="https://apsrcreatix.me">apsrcreatix</a> with <span role="img" aria-label="heart">❤️</span></strong>
