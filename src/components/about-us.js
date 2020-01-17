94f385b13d2ebf5fac3058ea2ad069c59c15c34c
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2020-01-17 13:23:14 +0530

    tweaked

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -26,7 +26,7 @@ function AboutUs() {
                         <div className="column">
                             <h6 className="title is-6">Rajesh Rajput</h6>
                             <p className="subtitle is-6">Professional Photographer</p>
-                            <p className="subtitle is-4">Hi <span role="img" aria-label="wave hands">👋</span>, I am a professional photographer who works on weekdays as a <a href="https://www.linkedin.com/in/irajeshrajput" target="_blank" >Senior iOS Developer</a> at IBM. I have been doing this since 2015 and love it. I feel glorious and motivated when somebody uses photos clicked by me for their digital profile pictures.</p>
+                            <p className="subtitle is-4">Hi <span role="img" aria-label="wave hands">👋</span>, I am a <a href="https://unsplash.com/@rrajputphotography" target="_blank" rel="noopener noreferrer">crafted photographer</a> who works on weekdays as a <a href="https://www.linkedin.com/in/irajeshrajput" target="_blank" rel="noopener noreferrer">Senior iOS Developer</a> at IBM. I have been doing this since 2015 and love it. I feel glorious and motivated when somebody uses photos clicked by me for their digital profile pictures.</p>
                         </div>
                     </div>
                 </div>
@@ -48,7 +48,7 @@ function AboutUs() {
                         <div className="column">
                             <h6 className="title is-6">Ranjana Rajput</h6>
                             <p className="subtitle is-6">Leads and Content Manager</p>
-                            <p className="subtitle is-4">Namastay <span role="img" aria-label="zangy face">🤪</span>, I am a <a href="https://unsplash.com/@ranjanarajput">junior photographer</a> who likes to click nature (mostly environment and animals) and busy completing my graduation these days. I do and learning content writing, website development, digital marketing content generation, and many more. Also, am planning to start something of my own sooner or later. </p>
+                            <p className="subtitle is-4">Namastay <span role="img" aria-label="zangy face">🤪</span>, I am a <a href="https://unsplash.com/@ranjanarajput" target="blank" rel="noopener noreferrer">junior photographer</a> who likes to click nature (mostly environment and animals) and busy completing my graduation these days. I do and learning content writing, website development, digital marketing content generation, and many more. Also, am planning to start something of my own sooner or later. </p>
                         </div>
                     </div>
                 </div>
